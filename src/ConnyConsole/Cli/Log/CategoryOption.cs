804bbe126a9e33f6b9ba291dc19a277c67ab91cd
Christopher Wolf <4867233+HaGGi13@users.noreply.github.com>
2025-06-09 18:34:26 +0200

    Update .gitignore to include CLI log command code
    Item: #21

---
+++ src/ConnyConsole/Cli/Log/CategoryOption.cs
@@ -0,0 +1,14 @@
+﻿using System.CommandLine;
+using Microsoft.Extensions.Logging;
+
+namespace ConnyConsole.Cli.Log;
+
+public sealed class CategoryOption : Option<LogLevel>
+{
+    public CategoryOption() : base(name: "--category",
+        description: "The log category.",
+        getDefaultValue: () => LogLevel.Information)
+    {
+        AddAlias("-c");
+    }
+}
---
+++ src/ConnyConsole/Cli/Log/MessageArgument.cs
@@ -0,0 +1,7 @@
+﻿using System.CommandLine;
+
+namespace ConnyConsole.Cli.Log;
+
+public sealed class MessageArgument() : Argument<string>("message", "The message to write.")
+{
+}
